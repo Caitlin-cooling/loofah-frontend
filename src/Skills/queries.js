3194cf95388f728a5143ead7858d37a6319ffe77
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-11-23 18:45:57 +0000

    LOOFAH-62: Implements apollo cache

--- src/Skills/queries.js
+++ src/Skills/queries.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-boost';
+import { gql } from '@apollo/client';
 
 export const GET_SKILLS_QUERY = gql`
   query Skills($filter: SkillFilter) {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { loofahApiUrl } from './config/config';
 
-const client = new ApolloClient({
-  uri: `${loofahApiUrl}`
+export const client = new ApolloClient({
+  uri: `${loofahApiUrl}`,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
