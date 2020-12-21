295e52456ac97ae8e4008ea8819767ad5192b79e
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-12-21 16:08:50 +0000

    LFH-58: Styling changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import './App.css';
 import Home from './Home';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <AppBar color="inherit">
-          <Toolbar>
-            <Typography variant="h6">
-              LOOFAH
-            </Typography>
-          </Toolbar>
-        </AppBar>
         <Switch>
           <Route exact path="/">
             <Home />
