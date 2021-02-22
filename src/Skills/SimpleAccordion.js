dfd72084490021270c2e223e2dc3ccaaedc5f069
Caitlin-cooling <ccooling@deloitte.co.uk>
2021-02-22 19:28:29 +0000

    LFH-71 implements skills accordion

---
+++ src/Skills/SimpleAccordion.js
@@ -0,0 +1,152 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import grey from "@material-ui/core/colors/grey";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  List,
+  ListItem,
+  ListItemText,
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+  Typography
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { startCase, range } from "lodash";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  accordion: {
+    boxShadow: "none",
+    border: `${grey[300]} solid 1px`,
+    borderRadius: "4px"
+  },
+  summary: {
+    borderBottom: `${grey[300]} solid 3px`
+  },
+  grade: {
+    padding: `${theme.spacing(2)}px 0 ${theme.spacing(2)}px ${theme.spacing(2)}px`,
+    borderBottom: `${grey[300]} solid 1px`
+  },
+  skills: {
+    paddingLeft: 0,
+    paddingRight: 0,
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    width: "auto"
+  },
+  borderBottom: {
+    borderBottom: `${grey[300]} solid 1px`
+  },
+  details: {
+    paddingLeft: 0,
+    width: "100%"
+  },
+  list: {
+    width: "100%"
+  },
+  expandAllSwitch: {
+    alignItems: "flex-end"
+  }
+}));
+
+export default function SimpleAccordion({ skills }) {
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    expanded: []
+  });
+
+  const handleExpandAll = (event) => {
+    if (event.target.checked) {
+      setState({ ...state, expanded: range(Object.keys(skills).length)});
+    } else {
+      setState({ ...state, expanded: []});
+    }
+  };
+
+  const handleExpandSingle = (index) => () => {
+    if (state.expanded.includes(index)) {
+      const newArray = state.expanded.filter((element) => element !== index);
+      setState({ ...state, expanded: newArray});
+    } else {
+      setState({ ...state, expanded: [...state.expanded, index]});
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.expandAllSwitch}>
+        <FormGroup aria-label="expand all" row>
+          <FormControlLabel
+            value="top"
+            control={
+              <Switch
+                color="default"
+                checked={state.showAll}
+                onChange={handleExpandAll}
+                name="showAll"
+              />
+            }
+            label="Expand all"
+            labelPlacement="top"
+          />
+        </FormGroup>
+      </FormControl>
+      {Object.keys(skills).map((title, index) => {
+        return <Accordion
+          className={classes.accordion}
+          key={index}
+          expanded={state.expanded.includes(index)}
+          onChange={handleExpandSingle(index)}
+        >
+          <AccordionSummary
+              className={classes.summary}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`accordion${index}-content`}
+              id={`accordion${index}-header`}
+            >
+              <Typography variant="h5" component="h5">
+                {startCase(title)}
+              </Typography>
+            </AccordionSummary>
+          <AccordionDetails className={classes.details}>
+              <List className={classes.list}>
+                {Object.keys(skills[title]).map((grade) => {
+                    return <div key={grade}>
+                      <Typography variant="h6" component="h6" className={classes.grade}>
+                        {startCase(grade)}
+                      </Typography>
+                      {skills[title][grade].map((skill, index) => {
+                        return <ListItem
+                          key={skill.id}
+                          className={
+                            classes.skills
+                            + " "
+                            + (index + 1 >= skills[title][grade].length ? "" : classes.borderBottom)
+                          }
+                        >
+                          <ListItemText primary={skill.description} />
+                        </ListItem>;
+                      })}
+                    </div>;
+                  })}
+              </List>
+          </AccordionDetails>
+        </Accordion>;
+      })}
+    </div>
+  );
+}
+
+SimpleAccordion.propTypes = {
+  skills: PropTypes.object.isRequired
+};
--- src/utils/__tests__/formatters.spec.js
+++ src/utils/__tests__/formatters.spec.js
@@ -1,12 +1,12 @@
-import { groupSkillsByTitle } from "../formatters";
+import { groupSkillsByTitleAndGrade } from "../formatters";
 
-describe("formatters.groupSkillsByTitle", () => {
+describe("formatters.groupSkillsByTitleAndGrade", () => {
   it("returns null if no skills are passed in", () => {
-    expect(groupSkillsByTitle()).toEqual(null);
+    expect(groupSkillsByTitleAndGrade()).toEqual(null);
   });
 
-  it("returns correctly grouped and ordered skills", () => {
-    const result = groupSkillsByTitle([
+  it("returns correctly grouped skills", () => {
+    const result = groupSkillsByTitleAndGrade([
       {
         category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
         description: "You understand a wide range of design patterns and how they solve common recurring problems",
@@ -18,7 +18,7 @@ describe("formatters.groupSkillsByTitle", () => {
       {
         category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
         description: "You understand algorithmic (time and space) and code (cyclomatic) complexity",
-        grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
+        grade: {id: "5feca323bce133e744d09b2d", title: "developer", __typename: "Grade"},
         id: "5feca323bc67214a38e4070d",
         title: "algorithms and data structures",
         __typename: "SkillDTO"
@@ -26,7 +26,7 @@ describe("formatters.groupSkillsByTitle", () => {
       {
         category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
         description: "You understand a wide range of design patterns and how they solve common recurring problems",
-        grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
+        grade: {id: "5feca323bce133e744d09b2d", title: "developer", __typename: "Grade"},
         id: "5feca323bc67214a38e40710",
         title: "code quality",
         __typename: "SkillDTO"
@@ -41,45 +41,53 @@ describe("formatters.groupSkillsByTitle", () => {
       }
     ]);
     expect(result).toEqual({
-      "algorithms and data structures": [
-        {
-          category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
-          description: "You understand algorithmic (time and space) and code (cyclomatic) complexity",
-          grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
-          id: "5feca323bc67214a38e4070d",
-          title: "algorithms and data structures",
-          __typename: "SkillDTO"
-        },
-        {
-          category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
-          description: "You have an awareness of common data structures and their benefits / limitations",
-          grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
-          id: "5feca323bc67214a38e4070e",
-          title: "algorithms and data structures",
-          __typename: "SkillDTO"
-        },
-      ],
-      "code quality": [
-        {
-          category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
-          description: "You understand a wide range of design patterns and how they solve common recurring problems",
-          grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
-          id: "5feca323bc67214a38e40710",
-          title: "code quality",
-          __typename: "SkillDTO"
-        }
-      ],
-      "running and deploying": [
-        {
-          category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
-          description: "You understand a wide range of design patterns and how they solve common recurring problems",
-          grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
-          id: "5feca323bc67214a38e40710",
-          title: "running and deploying",
-          __typename: "SkillDTO"
-        },
-      ]
+      "algorithms and data structures": {
+        "analystDeveloper": [
+          {
+            category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
+            description: "You have an awareness of common data structures and their benefits / limitations",
+            grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
+            id: "5feca323bc67214a38e4070e",
+            title: "algorithms and data structures",
+            __typename: "SkillDTO"
+          }
+        ],
+        "developer": [
+          {
+            category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
+            description: "You understand algorithmic (time and space) and code (cyclomatic) complexity",
+            grade: {id: "5feca323bce133e744d09b2d", title: "developer", __typename: "Grade"},
+            id: "5feca323bc67214a38e4070d",
+            title: "algorithms and data structures",
+            __typename: "SkillDTO"
+          }
+        ]
+      },
+      "code quality": {
+        "developer": [
+          {
+            category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
+            description: "You understand a wide range of design patterns and how they solve common recurring problems",
+            grade: {id: "5feca323bce133e744d09b2d", title: "developer", __typename: "Grade"},
+            id: "5feca323bc67214a38e40710",
+            title: "code quality",
+            __typename: "SkillDTO"
+          }
+        ]
+      },
+      "running and deploying": {
+        "analystDeveloper": [
+          {
+            category: {id: "5feca3235c775b1ddfe93a68", title: "technical", __typename: "Category"},
+            description:
+              "You understand a wide range of design patterns and how they solve common recurring problems",
+            grade: {id: "5feca323bce133e744d09b2d", title: "analystDeveloper", __typename: "Grade"},
+            id: "5feca323bc67214a38e40710",
+            title: "running and deploying",
+            __typename: "SkillDTO"
+          }
+        ]
+      },
     });
-    expect(Object.keys(result)).toEqual(["algorithms and data structures", "code quality", "running and deploying"]);
   });
 });
