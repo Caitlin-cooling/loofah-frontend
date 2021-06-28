d53d592c1212d57e4acc0b4fc41d61c0d462310a
Caitlin-cooling <ccooling@deloitte.co.uk>
2021-06-28 17:10:58 +0100

    LFH-97 Adds burger menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
 import Skills from "./pages/Skills";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import {
   ThemeProvider,
   Typography,
   AppBar,
   Toolbar,
-  Divider
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 import { makeStyles, createMuiTheme } from "@material-ui/core/styles";
 import logo from "./assets/Deloitte-Digital-Logo.png";
 
@@ -45,27 +51,88 @@ export const theme = createMuiTheme({
   }
 });
 
+const barHeight = 110;
+
 const useStyles = makeStyles({
   logo: {
     height: theme.spacing(8)
+  },
+  logoAndLoofah: {
+    display: "flex",
+    alignItems: "center"
   },
   appBar: {
     padding: theme.spacing(3),
     backgroundColor: "white",
     color: "black",
-    zIndex: theme.zIndex.drawer + 1
+    zIndex: theme.zIndex.drawer + 1,
+    height: `${barHeight}px`
   },
   toolBar: {
-    width: "95%",
-    margin: "auto"
+    width: "100%",
+    margin: "auto",
+    justifyContent: "space-between"
   },
   divider: {
     margin: theme.spacing(1)
+  },
+  drawer: {
+    flexShrink: 0
+  },
+  drawerPaper: {
+    [theme.breakpoints.down("sm")]: {
+    width: "100%"
+    },
+    [theme.breakpoints.up("md")]: {
+    width: "15%"
+    },
+    zIndex: theme.zIndex.drawer + 2
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    height: `${barHeight}px`,
+    [theme.breakpoints.down("sm")]: {
+      padding: theme.spacing(0, 2)
+      },
+    [theme.breakpoints.up("md")]: {
+      margin: theme.spacing(0, 6)
+    }
+  },
+  list: {
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: theme.spacing(2)
+      },
+    [theme.breakpoints.up("md")]: {
+      paddingRight: theme.spacing(9)
+    }
+  },
+  link: {
+    fontSize: "1.4rem",
+    fontWeight: "bold"
+  },
+  menuIconWrapper: {
+    [theme.breakpoints.up("md")]: {
+      margin: theme.spacing(0, 6)
+    }
+  },
+  menuIcon: {
+    fontSize: theme.spacing(5)
   }
 });
 
 const App = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Router>
@@ -73,11 +140,25 @@ const App = () => {
         <div className="App">
         <AppBar className={classes.appBar} position="fixed">
           <Toolbar className={classes.toolBar}>
-            <img src={logo} alt="Deloitte Digital logo" className={classes.logo} />
-            <Divider orientation="vertical" flexItem className={classes.divider} />
-            <Typography variant="h6">
-              LOOFAH
-            </Typography>
+            <div className={classes.logoAndLoofah}>
+              <Link to={"/"}>
+                <img src={logo} alt="Deloitte Digital logo" className={classes.logo} />
+              </Link>
+              <Divider orientation="vertical" flexItem className={classes.divider} />
+              <Link to={"/"}>
+                <Typography variant="h6">
+                  LOOFAH
+                </Typography>
+              </Link>
+            </div>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={classes.menuIconWrapper}
+            >
+              <MenuIcon className={classes.menuIcon} />
+            </IconButton>
           </Toolbar>
         </AppBar>
           <Switch>
@@ -88,6 +169,36 @@ const App = () => {
             </Route>
           </Switch>
         </div>
+          <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <List className={classes.list}>
+            {[
+              { text: "Browse Skills", path: "/skills" }
+            ].map((link) => (
+              <ListItem button key={link.text}>
+                <Link
+                  to={link.path}
+                  className={classes.link}
+                  onClick={handleDrawerClose}
+                >
+                  {link.text}
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
       </ThemeProvider >
     </Router>
   );
