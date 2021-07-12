f79d43b9fe88e969f084dd4872077ab2a8c6006e
Caitlin-cooling <ccooling@deloitte.co.uk>
2021-07-12 16:11:14 +0100

    LFH-129 adds see more links

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
-import { Link } from "react-router-dom";
-import { Typography, Toolbar, Tooltip } from "@material-ui/core";
+import {
+  Typography,
+  Toolbar
+} from "@material-ui/core";
+import kebabCase from "lodash/kebabCase";
+import { HashLink as Link } from "react-router-hash-link";
 import { makeStyles } from "@material-ui/core/styles";
 import { GET_GRADES_QUERY, GET_CRAFTS_QUERY } from "../../queries";
 import { DEFAULT_GRADE } from "../../data";
@@ -31,6 +35,18 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "60%",
     margin: "auto",
     padding: theme.spacing(3)
+  },
+  heading: {
+    padding: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px 0`
+  },
+  paragraph: {
+    fontSize: "1.2rem",
+    lineHeight: "1.56",
+    paddingBottom: theme.spacing(2)
+  },
+  seeMore: {
+    color: "#0E5B7C",
+    textDecoration: "underline"
   },
   skillsAndFilters: {
     position: "relative",
@@ -81,7 +97,12 @@ const Skills = () => {
     });
   }
 
-  if (gradesLoading || craftsLoading) return <p>Loading...</p>;
+  function getSeeMoreLink(gradeTitle) {
+    return `/pathway#${kebabCase(gradeTitle)}`;
+  }
+
+  if (gradesLoading || craftsLoading)
+    return <p>Loading...</p>;
   if (gradesError || craftsError) return <p>Error</p>;
 
   const orderedCraftTitles = orderCraftTitles(craftsResponse.crafts);
@@ -91,28 +112,30 @@ const Skills = () => {
       <main className={classes.content}>
         <Toolbar className={classes.toolbar} />
         <Typography variant="h1">Engineering Skills</Typography>
-        <TabGroup
-          handleFilterChange={handleFilterChange}
-          listItems={gradesResponse.grades}
-          keyName="gradeTitles"
-        />
-        <Typography variant="body2" className={classes.paragraph}>
-          {getSelectedGradeByTitle(selectedGradeTitle).description}
-        </Typography>
-        <div className={classes.skillsAndFilters}>
-          <div className={classes.filters}>
-            <Tooltip title="See more information about the crafts" arrow>
+          <TabGroup
+            handleFilterChange={handleFilterChange}
+            listItems={gradesResponse.grades}
+            keyName="gradeTitles"
+            selectedGradeTitle={selectedGradeTitle}
+          />
+          <Typography variant="body2" className={classes.paragraph}>
+            { getSelectedGradeByTitle(selectedGradeTitle).description }
+          </Typography>
+          <Link to={getSeeMoreLink(selectedGradeTitle)} className={classes.seeMore}>
+            See more
+          </Link>
+          <div className={classes.skillsAndFilters}>
+            <div className={classes.filters}>
               <Typography variant="body1" className={classes.craftFilter}>
                 <span>By </span>
                 <Link to="/crafts">Craft:</Link>
               </Typography>
-            </Tooltip>
-            <ChipGroup
-              handleFilterChange={handleFilterChange}
-              chipItems={orderedCraftTitles}
-              keyName="craftTitles"
-              variant="outlined"
-            />
+              <ChipGroup
+                handleFilterChange={handleFilterChange}
+                chipItems={orderedCraftTitles}
+                keyName="craftTitles"
+                variant="outlined"
+              />
           </div>
           <SkillsList queryDetails={{ variables: { filter: queryFilter } }} />
         </div>
